3bed78bddc4bc761d13029f2fad7bf7297a6c9fe
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-09 13:07:29 -0500

    my solution to part 2 of expense tracker

--- arrays/expenseTracker.js
+++ arrays/expenseTracker.js
@@ -2,6 +2,7 @@
 const account = {
     name: "Stacy M",
     expenses: [],
+    income: [],
     // addExpense -> two arguements: the description, the amount (add new object on 
     // to expenses array with the correct data)
     addExpense: function (descrip, amount) {
@@ -10,21 +11,35 @@ const account = {
             amount: amount
         })
     },
+    // Set up and addIncome method whish will take in: desscription, amount
+    addIncome: function (descrip, amount) {
+        this.income.push({
+            description: descrip,
+            amount: amount
+        })
+    },
     // getAccountSummary -> total up all expenses and print msg: "Stacy M has $27 in expenses."
     // use forEach to go through every item in expenses array
     getAccountSummary: function (){
         let totalExpenses = 0
+        let totalIncome = 0
+
         this.expenses.forEach(function(expense){
             totalExpenses = totalExpenses + expense.amount
         })
+        this.income.forEach(function(income){
+            totalIncome = totalIncome + income.amount
+        })
 
-        return `${account.name} has $${totalExpenses} in expenses.`
-    }
+        return `${account.name} has a balance of $${totalIncome - totalExpenses}, with $${totalExpenses} in expenses and a total income of $${totalIncome}.`
+    },
+    
+
 }
-
-
 
 account.addExpense("Rent", 950)
 account.addExpense("Coffee", 2)
+account.addIncome("Job", 1000)
+account.addIncome("hustle", 225)
 // console.log(account)
 console.log(account.getAccountSummary())

9d17df3ac44af385e04ae07f6184be3b308b04e7
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-10 19:31:38 -0500

    my version where I didn't follow directions

--- toDoApp/toDoApp.js
+++ toDoApp/toDoApp.js
@@ -16,11 +16,18 @@ const toDos = [{
     completed: false
 }]
 
-const paragraphs = document.querySelectorAll("p")
+// Print summary message: "you have # to do's left" into p tag
+const toDoCount = document.createElement("p")
+toDoCount.textContent = `You have ${toDos.length} items left to do!`
+document.querySelector("body").appendChild(toDoCount)
 
-paragraphs.forEach(function(paragraph){
-    if (paragraph.textContent.includes("The")) {
-        paragraph.remove()
-    }
+// Print a p for each to do above (use the text value of the object as the text for p)
+toDos.forEach(function (p) {
+    const toDoItem = document.createElement("p")
+    // toDoItem.textContent = `${toDos.item}`
+    toDoItem.textContent = toDos.item
+    // p.innerHTML = toDos[item]
+    document.querySelector("body").appendChild(toDoItem)
 })
+
 

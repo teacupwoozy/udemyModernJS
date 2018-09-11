c598f9375c77931594cf44ad723afde3f0b42071
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-10 19:44:48 -0500

    list number of todos left and then list all todos on list

--- toDoApp/toDoApp.js
+++ toDoApp/toDoApp.js
@@ -12,22 +12,22 @@ const toDos = [{
     item: "buy chocolate",
     completed: true
 }, {
-    item: "Pack",
+    item: "pack",
     completed: false
 }]
 
 // Print summary message: "you have # to do's left" into p tag
-const toDoCount = document.createElement("p")
-toDoCount.textContent = `You have ${toDos.length} items left to do!`
-document.querySelector("body").appendChild(toDoCount)
-
-// Print a p for each to do above (use the text value of the object as the text for p)
-toDos.forEach(function (p) {
-    const toDoItem = document.createElement("p")
-    // toDoItem.textContent = `${toDos.item}`
-    toDoItem.textContent = toDos.item
-    // p.innerHTML = toDos[item]
-    document.querySelector("body").appendChild(toDoItem)
+const incompeteToDos = toDos.filter(function(todo){
+    return !todo.completed
 })
 
+const summary = document.createElement("h2")
+summary.textContent = `You have ${incompeteToDos.length} todos left.`
+document.querySelector("body").appendChild(summary)
 
+// Print a p for each to do above (use the text value of the object as the text for p)
+toDos.forEach(function (todo) {
+    const paragraph = document.createElement("p")
+    paragraph.textContent = todo.item
+    document.querySelector("body").appendChild(paragraph)
+})

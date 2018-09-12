2581a5e76de0a21e6866b52a216dd28b373800c3
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-12 18:03:50 -0500

    render filtered data

--- notesApp/notesApp.js
+++ notesApp/notesApp.js
@@ -9,6 +9,26 @@ const notes = [{
     body: "get an office"
 }]
 
+const filters = {
+    searchText: ""
+}
+
+const renderNotes = function(notes, filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector("#notes").innerHTML = ""
+
+    filteredNotes.forEach(function(note){
+        const noteEl = document.createElement("p")
+        noteEl.textContent = note.title
+        document.querySelector("#notes").appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
 document.querySelector("#create-note").addEventListener("click", function(e){
     e.target.textContent = "The button was clicked."
 })
@@ -20,5 +40,6 @@ document.querySelector("#remove-all").addEventListener("click", function(){
 })
 
 document.querySelector("#search-text").addEventListener("input", function(e){
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })

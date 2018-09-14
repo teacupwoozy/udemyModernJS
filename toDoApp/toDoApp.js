a89faa6df693891e1b56f2bfaad872c4e81cf88e
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-14 09:47:50 -0500

    add ability to filter results for both apps

--- toDoApp/toDoApp.js
+++ toDoApp/toDoApp.js
@@ -1,45 +1,56 @@
 // const toDo = ["study", "make pie", "dinner date", "buy chocolate", "pack"]
-const toDos = [{
+const todos = [{
     item: "study",
-    category: "school",
     completed: false
 }, {
     item: "make pie",
-    category: "home",
     completed: true
 }, {
     item: "dinner date",
-    category: "personal",
     completed: false
 }, {
     item: "buy chocolate",
-    category: "home",
     completed: true
 }, {
     item: "pack",
-    category: "personal",
     completed: false
 }, {
     item: "write program",
-    category: "school",
     completed: false
 }]
 
-// Print summary message: "you have # to do's left" into p tag
-const incompeteToDos = toDos.filter(function(todo){
-    return !todo.completed
-})
+// Set up filters (searchText) and wire up a new filter input to change it.
+const filters = {
+    searchText: ""
+}
+// Create a renderTodos function to render and rerender the latest filtered data.
+const renderTodos = function(todos, filters){
+    const filteredTodos = todos.filter(function(todo){
+        return todo.item.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
-const summary = document.createElement("h2")
-summary.textContent = `You have ${incompeteToDos.length} todos left.`
-document.querySelector("body").appendChild(summary)
+    // Print summary message: "you have # to do's left" into p tag
+    const incompeteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
 
-// Print a p for each to do above (use the text value of the object as the text for p)
-toDos.forEach(function (todo) {
-    const paragraph = document.createElement("p")
-    paragraph.textContent = todo.item
-    document.querySelector("body").appendChild(paragraph)
-})
+    document.querySelector("#todos").innerHTML = ""
+
+    const summary = document.createElement("h2")
+    summary.textContent = `You have ${incompeteTodos.length} todos left.`
+    document.querySelector("#todos").appendChild(summary)
+
+    // Print a p for each to do above (use the text value of the object as the text for p)
+    filteredTodos.forEach(function (todo) {
+        const paragraph = document.createElement("p")
+        paragraph.textContent = todo.item
+        document.querySelector("#todos").appendChild(paragraph)
+    })
+
+}
+
+renderTodos(todos, filters)
+
 
 // Listen for add to do text change.
 document.querySelector("#new-todo").addEventListener("input", function(e){
@@ -50,8 +61,7 @@ document.querySelector("#add-todo").addEventListener("click", function(){
     console.log("To Do button clicked!")
 })
 
-
-// List the count for each category
-
-
-// my own exercise: Print summary of how many items in each category are left to do
+document.querySelector("#search-text").addEventListener("input", function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})

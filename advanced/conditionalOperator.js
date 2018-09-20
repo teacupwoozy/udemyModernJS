d05febdc4fedb30cd1faaca3a5dfa1fcf9cc8c43
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-20 18:43:43 -0500

    Moar conditional exercises

--- advanced/conditionalOperator.js
+++ advanced/conditionalOperator.js
@@ -1,10 +1,14 @@
-const myAge = 7
-const message = myAge >= 18 ? "You can vote!" : "You cannot vote yet"
+// const myAge = 7
+// const message = myAge >= 18 ? "You can vote!" : "You cannot vote yet"
+// console.log(message)
 
-// if (myAge >= 18){
-//     message = "You can vote!"
-// } else {
-//     message = "You cannot vote yet."
-// }
+const myAge = 20
+const showPage = () => {
+    return "Showing the page"
+}
+const showErrorPage = () => {
+    return "Showing the error page"
+}
 
+const message = myAge >= 21 ? showPage() : showErrorPage()
 console.log(message)

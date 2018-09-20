29fc9409187e7048ad6432b4fe189860debade57
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-19 19:05:41 -0500

    add edit note page, add uuid to local, add id's to links

--- notesApp/notesApp.js
+++ notesApp/notesApp.js
@@ -7,13 +7,15 @@ const filters = {
 renderNotes(notes, filters)
 
 document.querySelector("#create-note").addEventListener("click", function(e){
+    const id = uuidv4()
     notes.push({
-        id: uuidv4(),
+        id: id,
         title: "",
         body: ""
     })
-    localStorage.setItem("notes", JSON.stringify(notes))
-    renderNotes(notes, filters)
+    // localStorage.setItem("notes", JSON.stringify(notes))
+    saveNotes(notes)
+    location.assign(`/edit.html#${id}`)
 })
 
 document.querySelector("#search-text").addEventListener("input", function(e){
--- notesApp/notesFunctions.js
+++ notesApp/notesFunctions.js
@@ -23,7 +23,7 @@ const removeNote = function(id){
 // Generate the DOM structure for a note
 const generateNoteDOM = function(note){
     const noteEl = document.createElement("div")
-    const textEl = document.createElement("span")
+    const textEl = document.createElement("a")
     const button = document.createElement("button")
 
     // Set up the remove note button
@@ -41,8 +41,8 @@ const generateNoteDOM = function(note){
     } else {
         textEl.textContent = "unnamed note"
     }
+    textEl.setAttribute("href", `/edit.html#${note.id}`)
     noteEl.appendChild(textEl)
-
     
     return noteEl
 }

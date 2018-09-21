e9208323fdf652de23fb6eec0acedb6c12b61209
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-21 10:56:52 -0500

    refactor with truthy and falsy

--- notesApp/notesFunctions.js
+++ notesApp/notesFunctions.js
@@ -1,7 +1,7 @@
 // Read existing notes from localStorgage
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem("notes")
-    return notesJSON !== null ? JSON.parse(notesJSON) : []
+    return notesJSON ? JSON.parse(notesJSON) : []
 }
 
 // Save the notes to localStorage

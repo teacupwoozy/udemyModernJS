374bbf9f7d032491cc0c5ddab559a1cb0694a231
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-21 12:38:11 -0500

    intro try catch exercise

--- advanced/tryCatch.js
+++ advanced/tryCatch.js
@@ -2,9 +2,14 @@ const getTip = (amount) => {
     if (typeof amount === "number"){
         return amount * .25
     } else {
-        throw "Argument must be a number."
+        throw Error("Argument must be a number.")
     }
 }
 
-const result = getTip(2)
-console.log(result)
+try {
+    const result = getTip(10)
+    console.log(result)
+} catch (e) {
+    console.log("catch block is running")
+}
+

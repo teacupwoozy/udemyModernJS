af356afa0abcb18aad59024430dc23adb16e223d
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-21 11:40:15 -0500

    type coersion exercise

---
+++ advanced/typeCoersion.js
@@ -0,0 +1,12 @@
+// type coersion: a string, a number, or a boolean
+
+// console.log("5" + 5)
+// console.log("5" - 5)
+// double equals does not check for type
+// console.log("5" == 5)
+
+const value = true + 12
+// To check the given type of an operator, use typeof
+const type = typeof value
+console.log(type)
+console.log(value)

31f5f597939f2cbe90b29f25fec9ce69cb2b097e
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-24 14:18:19 -0500

    initial commit of hangman game. includes hardcoded guesses, method for checking guesses against word.

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,25 @@
-const Hangman = function(word, guesses) {
-    this.word = word
-    this.guesses = guesses
+const Hangman = function(word, guessCount, letterGuessed) {
+    this.word = word.toLowerCase().split("")
+    this.guessCount = guessCount
+    this.letterGuessed = ["e", "b"]
 }
 
-const try1 = new Hangman("meow", 4)
-console.log(try1)
+Hangman.prototype.getPuzzle = function(){
+    let puzzle = ""
 
-const try2 = new Hangman("beepbeep", 5)
-console.log(try2)
+    this.word.forEach((letter) => {
+        if (this.letterGuessed.includes(letter) || letter === " ") {
+            puzzle += letter
+        } else {
+            puzzle += "*"
+        }
+    })
+
+    return puzzle
+}
+
+const try1 = new Hangman("MEOW", 4, ["a", "b", "c"])
+console.log(try1.getPuzzle())
+
+const try2 = new Hangman("beepbeep", 5, [])
+console.log(try2.getPuzzle())

96e605985fc4d940e32819861aed1c2832b99046
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-24 11:40:03 -0500

    add use strict

--- notesApp/notesFunctions.js
+++ notesApp/notesFunctions.js
@@ -1,7 +1,14 @@
+"use strict"
+
 // Read existing notes from localStorgage
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem("notes")
-    return notesJSON ? JSON.parse(notesJSON) : []
+
+    try {
+        return notesJSON ? JSON.parse(notesJSON) : []        
+    } catch (error) {
+        return []
+    }
 }
 
 // Save the notes to localStorage

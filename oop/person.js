51f347b72f77dd5c8b95d4f0dccd7f1780c57841
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-27 11:34:53 -0500

    add class syntax example to person.js

--- oop/person.js
+++ oop/person.js
@@ -1,30 +1,29 @@
-const Person = function (firstName, lastName, age, likes) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
+class Person {
+    constructor(firstName, lastName, age, likes) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio(){
+        let bio = `${this.firstName} is ${this.age}.`
+
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        return bio
+    }
+    setName(fullName) {
+        const names = fullName.split(" ")
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
-Person.prototype.getBio = function(){
-let bio = `${this.firstName} is ${this.age}.`
 
-this.likes.forEach((like) => {
-    bio += ` ${this.firstName} likes ${like}.`
-})
-return bio
-}
-
-Person.prototype.setName = function(fullName){
-    const names = fullName.split(" ")
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
 
 const me = new Person("Donna", "Washington", 57, ["reading", "kicking ass"])
 
-me.getBio = function (){
-    return "This is not real."
-}
 
 me.setName("Sophie Scholl")
 console.log(me.getBio())

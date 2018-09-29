133d756b5511d52c9623c3cbeb8653654e53732f
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-29 17:14:33 -0500

    refactor one function, add keyname req for wordcount on puzzle

--- hangman/app.js
+++ hangman/app.js
@@ -8,7 +8,7 @@ const try1 = new Hangman("beep beep", 4)
 puzzleEl.textContent = try1.puzzle
 guessesEl.textContent = try1.statusMessage
 
-window.addEventListener("keypress", function (e) {
+window.addEventListener("keypress", (e) => {
     const guess = String.fromCharCode(e.charCode)
     try1.makeGuess(guess)
     puzzleEl.textContent = try1.puzzle
@@ -20,11 +20,13 @@ window.addEventListener("keypress", function (e) {
 const request = new XMLHttpRequest()
 
 request.addEventListener("readystatechange", (e) => {
-    if(e.target.readyState === 4){
+    if(e.target.readyState === 4 && e.target.status === 200){
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+    } else if(e.target.readyState === 4) {
+        console.log("An error has taken place.")
     }
 }) 
 
-request.open("GET", "http://puzzle.mead.io/puzzle")
+request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3")
 request.send()

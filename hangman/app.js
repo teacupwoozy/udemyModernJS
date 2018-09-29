504e2a00b843618c012fe7ceecdefeb199dcbc8a
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-09-29 18:51:03 -0500

    insturctor solution, more succinct

--- hangman/app.js
+++ hangman/app.js
@@ -38,10 +38,8 @@ const countryRequest = new XMLHttpRequest()
 countryRequest.addEventListener("readystatechange", (e) => {
     if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
-        const isMexico = function (countries) {
-            return countries.alpha2Code === countryCode
-        }
-        console.log(data.find(isMexico).name)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
     } else if (e.target.readyState === 4) {
         console.log("There's an oopsie.")
     }

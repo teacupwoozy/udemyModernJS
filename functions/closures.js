8fcb9800a69640da682f051f535131a2b0ee5104
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-10-01 18:17:06 -0500

    add closure examples including getTip

--- functions/closures.js
+++ functions/closures.js
@@ -1,10 +1,50 @@
-const myFunction = () => {
-    const message = "This is my message."
-    const printMessage = () => {
-        console.log(message)
+const createCounter = () => {
+    let count = 0
+
+
+    return {
+        increment() {
+            count++
+        },
+        decrement() {
+            count--
+        },
+        get() {
+            return count
+        }
     }
-    return printMessage
+}
+    
+const counter = createCounter()
+counter.increment()
+counter.decrement()
+counter.decrement()
+console.log(counter.get())
+
+
+// adder
+const createAdder = (a) => {
+    return (b) => {
+        return a + b
+    }
 }
 
-const myPrintMessage = myFunction()
-myPrintMessage()
+const addTen = createAdder(10)
+console.log(addTen(-2))
+console.log(addTen(20))
+const add100 = createAdder(100)
+console.log(add100(-90))
+
+
+
+// tipper
+const createTipper = (tip) => {
+    return (bill) => {
+        return tip * bill
+    }
+}
+
+const tip15 = createTipper(.15)
+const tip20 = createTipper(.2)
+console.log(tip15(100))
+console.log(tip20(100))

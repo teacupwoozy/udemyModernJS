3bdac9aff6dc13c39e9d4a8a0e348d9cc381590e
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-10-01 20:24:56 -0500

    convert getPuzzle to promise

--- functions/promises.js
+++ functions/promises.js
@@ -15,13 +15,14 @@ getDataCallback((err, data) => {
 
 
 // promise
-const myPromise = new Promise((resolve, reject) => {
+const getDataPromise = (data) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve("This is the promise data.")
-        reject("This is the promise error.")
+        resolve(`This is my success data: ${data}`)
+        // reject("This is the promise error.")
     }, 2000);
 })
 
+const myPromise = getDataPromise(123)
 myPromise.then((data) => {
     console.log(data)
 }, (err) => {

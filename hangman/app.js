0b95fd5b1d13e8ca681cd681ec8b06f8e08933f5
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-10-02 18:47:35 -0500

    link up data to browser

--- hangman/app.js
+++ hangman/app.js
@@ -3,27 +3,37 @@
 
 const puzzleEl = document.querySelector("#puzzle")
 const guessesEl = document.querySelector("#guesses")
-const try1 = new Hangman("beep beep", 4)
-
-puzzleEl.textContent = try1.puzzle
-guessesEl.textContent = try1.statusMessage
+let try1
 
 window.addEventListener("keypress", (e) => {
     const guess = String.fromCharCode(e.charCode)
     try1.makeGuess(guess)
+    render()
+})
+
+const render = () => {
     puzzleEl.textContent = try1.puzzle
     guessesEl.textContent = try1.statusMessage
+}
 
-})
+const startGame = async () => {
+    const puzzle = await getPuzzle("2")
+    try1 = new Hangman(puzzle, 5)
+    render()
+}
 
-getPuzzle("2").then((puzzle) => {
-    console.log(puzzle)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-})
+document.querySelector("#reset").addEventListener("click", startGame)
 
-getCurrentCountry().then((country) => {
-    console.log(country.name)
-}).catch((error) => {
-    console.log(error)
-})
+startGame()
+
+// getPuzzle("2").then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// })
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name)
+// }).catch((error) => {
+//     console.log(error)
+// })

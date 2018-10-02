7c28fd0e070aadbb09cfcf9fc10fdd064c99db6d
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-10-02 14:27:57 -0500

    more promise chaining examples, fetch example and add promise chain and fetch to hangman's getPuzzle

--- functions/promises.js
+++ functions/promises.js
@@ -42,10 +42,10 @@ getDataPromise(2).then((data) => {
 })
 
 // promise chaining
-getDataPromise("10").then((data) => {
+getDataPromise(10).then((data) => {
     return getDataPromise(data)
 }).then((data) => {
-    return getDataPromise(data)
+    return "This is some test data."
 }).then ((data) => {
     console.log(data)
 }).catch((err) => {

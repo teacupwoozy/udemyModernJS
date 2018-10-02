a0cdff8854214ac3d8afbfb28a17ec4da6e06063
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-10-02 17:55:11 -0500

    convert getPuzzle to async await

--- hangman/requests.js
+++ hangman/requests.js
@@ -1,14 +1,13 @@
 // Making an HTTP request
-const getPuzzle = (wordCount) => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-        if(response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error("Unable to fetch puzzle.")
-        }
-    }).then((data) => {
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+
+    if(response.status === 200) {
+        const data = await response.json()
         return data.puzzle
-    })
+    } else {
+        throw new Error("Unable to get puzzle.")
+    }
 }
 
 const getCountry = (countryCode) => {

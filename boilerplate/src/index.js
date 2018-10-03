fc9ce1ab8a14e2caf00c1501cba47d5da48d6dda
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-10-03 18:41:37 -0500

    add babel exercises

---
+++ boilerplate/src/index.js
@@ -0,0 +1,12 @@
+const name = "Janet";
+
+console.log(name);
+
+class Hangman {
+    myMethod() {
+        return "Testing"
+    }
+}
+
+const hangman = new Hangman()
+console.log(hangman.myMethod())
